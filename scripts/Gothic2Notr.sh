9fb71fbe4aff6fd9589fb246e481e0a963bdd542
Sebastian Lindgren <sebbestune@icloud.com>
2020-07-07 16:57:33 +0300

    Update Gothic2Notr.sh
    This seem to work on Linux (Manjaro 20.0.3). There is no bin/ folder in the build file.

--- scripts/Gothic2Notr.sh
+++ scripts/Gothic2Notr.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
 if [[ $DEBUGGER != "" ]]; then
-  exec $DEBUGGER --args "$DIR/bin/Gothic2Notr" "$@"
+  exec $DEBUGGER --args "$DIR/Gothic2Notr" "$@"
 else
-  exec "$DIR/bin/Gothic2Notr" "$@"
+  exec "$DIR/Gothic2Notr" "$@"
 fi
